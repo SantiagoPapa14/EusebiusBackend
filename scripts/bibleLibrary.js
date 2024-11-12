4a836444e4c18592e3d5ddbe6099d9ac1f10e3b5
Santiago Papa <papa.santiago321@gmail.com>
2024-11-12 01:51:03 -0300

    Fixed reference parsing

--- scripts/bibleLibrary.js
+++ scripts/bibleLibrary.js
@@ -1,11 +1,21 @@
 const { queryDatabase } = require("../managers/sqliteAsyncManager");
 
 const fetchVerses = async (reading, language) => {
-  const allRows = await queryDatabase(
-    `SELECT * FROM ${language}Bible WHERE book = ? AND chapter = ? AND verse BETWEEN ? AND ?`,
-    [reading.book, reading.chapter, reading.verses.start, reading.verses.end]
-  );
-  return allRows;
+  if (!reading || reading.verses.length === 0) {
+    return null;
+  }
+
+  const result = [];
+
+  for (const verse of reading.verses) {
+    const allRows = await queryDatabase(
+      `SELECT * FROM ${language}Bible WHERE book = ? AND chapter = ? AND verse BETWEEN ? AND ?`,
+      [reading.book, verse.chapter, verse.start, verse.end]
+    );
+    result.push(...allRows);
+  }
+
+  return result;
 };
 
 module.exports = { fetchVerses };
--- scripts/readingScraper.js
+++ scripts/readingScraper.js
@@ -1,20 +1,49 @@
 function parseReference(reference) {
-  if (reference) {
-    const regex = /(\d*\s*[A-Za-z]+\s*[A-Za-z]+)\s+(\d+):(\d+)(?:-(\d+))?/;
-    const match = reference.match(regex);
+  if (!reference) {
+    return null;
+  }
+  const parsed = {
+    book: "",
+    verses: [],
+  };
+  const match = reference.match(/^([\w\s]+) (\d+:\d.*)$/);
+  parsed.book = match[1];
 
-    if (match) {
-      return {
-        book: match[1].trim().replace("First", "1").replace("Second", "2"),
-        chapter: parseInt(match[2], 10),
-        verses: {
-          start: parseInt(match[3], 10),
-          end: match[4] ? parseInt(match[4], 10) : parseInt(match[3], 10),
-        },
-      };
+  //Multiple verses in same chapter
+  if (match[2].includes(",")) {
+    const chapter = match[2].split(":")[0];
+    const readings = match[2].split(":")[1].split(",");
+    readings.forEach((reading) => {
+      const [start, end] = reading.split("-");
+      parsed["verses"].push({
+        chapter: Number(chapter),
+        start: Number(start),
+        end: end ? Number(end) : Number(start),
+      });
+    });
+  } else {
+    const readings = match[2].split("-");
+    if (readings[0].includes(":") && readings[1].includes(":")) {
+      parsed["verses"].push({
+        chapter: Number(readings[0].split(":")[0]),
+        start: Number(readings[0].split(":")[1]),
+        end: 999,
+      });
+      parsed["verses"].push({
+        chapter: Number(readings[1].split(":")[0]),
+        start: 1,
+        end: Number(readings[0].split(":")[1]),
+      });
+    } else {
+      parsed["verses"].push({
+        chapter: Number(readings[0].split(":")[0]),
+        start: Number(readings[0].split(":")[1]),
+        end: Number(readings[1]),
+      });
     }
   }
-  return null;
+
+  return parsed;
 }
 
 async function getReadingByItself() {
